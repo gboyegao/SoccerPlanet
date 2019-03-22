0f3a0c4c7f4b9133d4457d74c0b17f8e726f5400
Adegboyega Olusunmade <adegboyega@adegboyegas-macbook-pro.local>
2019-03-22 15:45:37 +0100

    Initial Commit

---
+++ SoccerPlanet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoccerPlanet
+//
+//  Created by Adegboyega Olusunmade on 22/03/2019.
+//  Copyright © 2019 Adegboyega Olusunmade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
